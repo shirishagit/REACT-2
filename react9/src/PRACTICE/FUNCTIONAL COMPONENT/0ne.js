2407a60af565e21f771a2933f1430655c692bf10
basani.shirisha <basani.shirisha@basanishirishas-MacBook-Air.local>
2025-01-20 22:55:20 +0530

    file changed

--- react9/src/PRACTICE/FUNCTIONAL COMPONENT/0ne.js
+++ react9/src/PRACTICE/FUNCTIONAL COMPONENT/0ne.js
@@ -3,14 +3,14 @@ import react, { use, useState } from "react";
 const One = () =>{
   const[data,update] =useState("shirisha")
     function dataUpdate(){
-        update("sai")
+        update("9sai")
     }
     return(
        <div>
-        <h1>{data}</h1>
+        <h1>The data will change on the click on the button this is call changing the state of functional compounent <br/>
+         Here is the name which will change on the click :-{data}</h1>
         <button onClick={dataUpdate}>click it</button>
-        
-       </div>
+        </div>
     )
   
 }
