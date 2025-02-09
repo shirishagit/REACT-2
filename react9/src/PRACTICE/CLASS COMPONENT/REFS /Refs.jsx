7cd3291c8e62f01e92ddbed26751422461dae57b
basani.shirisha <shirisha.baasani@gmail.com>
2025-02-09 23:26:07 +0530

    updated files

--- react9/src/PRACTICE/CLASS COMPONENT/REFS /Refs.jsx
+++ react9/src/PRACTICE/CLASS COMPONENT/REFS /Refs.jsx
@@ -3,18 +3,35 @@ import React,{Component} from "react";
 class Refs extends Component{
     constructor(){
         super()
+        this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setRef = element =>{
+            this.cbRef = element
+        }
     }
-    onfocseHandler(){
-    
+    componentDidMount(){
+        if(this.cbRef){
+            this.cbRef.focus()
+        }
     }
+
+    // componentDidMount(){
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
+    // }
     render(){
         return(
             <div>
-                <label>Username</label>
-                <input ref={this.onfocseHandler}/>
+                {/* <label>Username</label> */}
+                {/* <input ref={this.inputRef}/> */}
+                <label>id</label>
+                <input type="text"  ref={this.setRef}/>
             </div>
         )
     }
-}
+
+    }
+ 
+
 
 export default Refs;
